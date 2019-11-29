7b2be977faaa984611cf6bb80b8be9c17002292c
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-29 17:49:34 +0100

    create search feild class, with state handle submit event

--- src/components/SearchFeild.jsx
+++ src/components/SearchFeild.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import '../styles/SearchFeild.css';
 
-function SearchFeild() {
-  return (
-    <div className="app-body-search">
-        <h2>Github username</h2>
-        <form>
-            <input></input>
-            <button>generate</button>
-        </form>
-    </div>
-  );
+// Search feild create github api call from search input form
+class SearchFeild extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {results: []};
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount(){
+      // document.querySelector()
+    }
+
+    handleSubmit(e){
+      e.preventDefault();
+      console.log("search api", e)
+      const data = new FormData(e.target);
+      
+      fetch('/api/form-submit-url', {
+        method: 'POST',
+        body: data,
+      });
+      
+    }
+
+
+
+  render(){ // handle API data here. render each array object into DOM elements
+    return (
+      <div className="app-body-search">
+          <h2>Github username</h2>
+          <form onSubmit={this.handleSubmit}>
+              <input className="username" name="username" type="text"></input>
+              <button> generate </button>
+          </form>
+      </div>
+    );
+}
 }
 
 export default SearchFeild;
