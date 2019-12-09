df10b3f93e94b53c8413eff821dd7ddfbc407782
Colin Rosati <colin.james.rosati@gmail.com>
2019-12-09 15:31:05 +0100

    Updates

---
+++ precache-manifest.5b7f1b10b2328b4cfca3b0c17cd87d11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a567ad8540e57f984364611147f50087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32e8a4de5824557e19fc",
+    "url": "/static/css/main.62370633.chunk.css"
+  },
+  {
+    "revision": "88ad2a7a971ebc75c5b4",
+    "url": "/static/js/2.212a988c.chunk.js"
+  },
+  {
+    "revision": "32e8a4de5824557e19fc",
+    "url": "/static/js/main.18282dff.chunk.js"
+  },
+  {
+    "revision": "c1bb79c79efdbb8bbfbb",
+    "url": "/static/js/runtime-main.a65bd5b4.js"
+  }
+]);
